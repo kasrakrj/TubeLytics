163500ec0a0a01df6ccb213969acde3802054834
Mojtaba Peyrovian <m.peyrovian@outlook.com>
2024-11-03 08:26:17 -0500

    Renaming

--- app/models/services/YouTubeService.java
+++ app/models/services/YouTubeService.java
@@ -4,8 +4,10 @@ import models.entities.Video;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.net.URLEncoder;
 import java.net.http.*;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
@@ -17,7 +19,8 @@ public class YouTubeService {
 
     // Method to call the YouTube API and process the response
     public CompletionStage<List<Video>> searchVideos(String keyword) {
-        String apiUrl = YOUTUBE_SEARCH_URL + keyword + "&key=" + API_KEY;
+        String encodedKeyword = URLEncoder.encode(keyword, StandardCharsets.UTF_8);
+        String apiUrl = YOUTUBE_SEARCH_URL + encodedKeyword + "&key=" + API_KEY;
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiUrl)).build();
