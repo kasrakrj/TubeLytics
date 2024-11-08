20a6592d59fa1da903aec549d0afede775b8f516
Zahra Rasoulifar <zahra.rasouli1999@gmail.com>
2024-11-08 02:56:24 -0500

    Added javadoc for Video and ChannelProfileService

--- app/models/entities/Video.java
+++ app/models/entities/Video.java
@@ -1,5 +1,8 @@
 package models.entities;
 
+/**
+ * Represents a YouTube video with associated metadata.
+ */
 public class Video {
     private String title;
     private String description;
@@ -9,9 +12,23 @@ public class Video {
     private String channelId;
     private String VideoURL;
 
+    /**
+     * Default constructor.
+     */
     public Video() {
     }
 
+    /**
+     * Constructs a new {@code Video} with the specified details.
+     *
+     * @param title         the title of the video
+     * @param description   the description of the video
+     * @param channelTitle  the title of the channel that uploaded the video
+     * @param thumbnailUrl  the URL of the video's thumbnail image
+     * @param videoId       the unique identifier of the video
+     * @param channelId     the unique identifier of the channel
+     * @param videoURL      the URL of the video
+     */
     public Video(String title, String description, String channelTitle, String thumbnailUrl, String videoId, String channelId, String videoURL) {
         this.title = title;
         this.description = description;
@@ -22,58 +39,128 @@ public class Video {
         this.VideoURL = videoURL;
     }
 
+    /**
+     * Returns the title of the video.
+     *
+     * @return the video's title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of the video.
+     *
+     * @param title the video's title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Returns the description of the video.
+     *
+     * @return the video's description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the description of the video.
+     *
+     * @param description the video's description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Returns the title of the channel that uploaded the video.
+     *
+     * @return the channel's title
+     */
     public String getChannelTitle() {
         return channelTitle;
     }
 
+    /**
+     * Sets the title of the channel that uploaded the video.
+     *
+     * @param channelTitle the channel's title
+     */
     public void setChannelTitle(String channelTitle) {
         this.channelTitle = channelTitle;
     }
 
+    /**
+     * Returns the URL of the video's thumbnail image.
+     *
+     * @return the thumbnail URL
+     */
     public String getThumbnailUrl() {
         return thumbnailUrl;
     }
 
+    /**
+     * Sets the URL of the video's thumbnail image.
+     *
+     * @param thumbnailUrl the thumbnail URL
+     */
     public void setThumbnailUrl(String thumbnailUrl) {
         this.thumbnailUrl = thumbnailUrl;
     }
 
+    /**
+     * Returns the unique identifier of the video.
+     *
+     * @return the video ID
+     */
     public String getVideoId() {
         return videoId;
     }
 
+    /**
+     * Sets the unique identifier of the video.
+     *
+     * @param videoId the video ID
+     */
     public void setVideoId(String videoId) {
         this.videoId = videoId;
     }
 
+    /**
+     * Returns the unique identifier of the channel.
+     *
+     * @return the channel ID
+     */
     public String getChannelId() {
         return channelId;
     }
 
+    /**
+     * Sets the unique identifier of the channel.
+     *
+     * @param channelId the channel ID
+     */
     public void setChannelId(String channelId) {
         this.channelId = channelId;
     }
 
+    /**
+     * Returns the URL of the video.
+     *
+     * @return the video URL
+     */
     public String getVideoURL() {
         return VideoURL;
     }
 
+    /**
+     * Sets the URL of the video.
+     *
+     * @param videoURL the video URL
+     */
     public void setVideoURL(String videoURL) {
         VideoURL = videoURL;
     }
--- app/models/services/ChannelProfileService.java
+++ app/models/services/ChannelProfileService.java
@@ -12,18 +12,46 @@ import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
+/**
+ * Service class for retrieving YouTube channel information and videos.
+ *
+ * <p>This service uses the YouTube Data API to fetch channel details and video lists.
+ * It depends on {@link YouTubeService} for API configurations such as base URL and API key.</p>
+ */
 public class ChannelProfileService {
     YouTubeService youTubeService;
 
+    /**
+     * Constructs a new {@code ChannelProfileService} with the specified {@link YouTubeService}.
+     *
+     * @param youTubeService the YouTube service providing API configurations
+     */
     @Inject
     public ChannelProfileService(YouTubeService youTubeService) {
         this.youTubeService = youTubeService;
     }
 
+    /**
+     * Creates a new {@link HttpClient} instance.
+     *
+     * @return a new {@link HttpClient}
+     */
     protected HttpClient createHttpClient() {
         return HttpClient.newHttpClient();
     }
 
+    /**
+     * Retrieves channel information for the specified channel ID.
+     *
+     * <p>This method sends an asynchronous HTTP GET request to the YouTube Data API's "search" endpoint,
+     * requesting the snippet part for the given channel ID. It returns a {@link CompletionStage}
+     * that will contain the channel's snippet information as a {@link JSONObject} upon completion.
+     * The {@code CompletionStage} may complete exceptionally if an error occurs during the HTTP request
+     * or JSON parsing.</p>
+     *
+     * @param channelId the YouTube channel ID
+     * @return a {@link CompletionStage} containing a {@link JSONObject} with the channel's snippet information
+     */
     public CompletionStage<JSONObject> getChannelInfo(String channelId) {
         String youtubeChannelUrl = youTubeService.getApiUrl() + "/search?part=snippet&type=video&";
         String apiUrl = youtubeChannelUrl + "channelId=" + channelId + "&key=" + youTubeService.getApiKey();
@@ -37,6 +65,19 @@ public class ChannelProfileService {
         });
     }
 
+    /**
+     * Retrieves a list of videos from the specified YouTube channel.
+     *
+     * <p>This method sends an asynchronous HTTP GET request to the YouTube Data API's "search" endpoint,
+     * requesting the snippet part for videos in the given channel ID. It returns a {@link CompletionStage}
+     * that will contain a {@link List} of {@link Video} objects upon completion.
+     * The {@code CompletionStage} may complete exceptionally if an error occurs during the HTTP request
+     * or JSON parsing.</p>
+     *
+     * @param channelId  the YouTube channel ID
+     * @param maxResults the maximum number of videos to retrieve
+     * @return a {@link CompletionStage} containing a {@link List} of {@link Video} objects
+     */
     public CompletionStage<List<Video>> getChannelVideos(String channelId, int maxResults) {
         String youtubeChannelVideosUrl = youTubeService.getApiUrl() + "/search?part=snippet&type=video&";
         String apiUrl = youtubeChannelVideosUrl + "channelId=" + channelId + "&maxResults=" + maxResults + "&key=" + youTubeService.getApiKey();
